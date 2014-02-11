041b240cbafffa9ec9cb13ee45e07dbc7d4196db
Dragonfire <bastian616-git@yahoo.de>
2014-02-11 12:13:44 +0100

    correct typo

--- model/setup_test_data.sql
+++ model/setup_test_data.sql
@@ -6,13 +6,14 @@ INSERT INTO clans_list ( clan_name, status, clan_tag ) VALUES
 ( 'Percival', 1, 'DND' ), 
 ( 'Revenant', 1, 'PwnStars' ), 
 
-INSERT INTO players_list (`status`,  `player_name`) VALUES
-( 1, 'Lara Croft' ),
-( 1, 'Max Payne' ),( 0, 'Donkey Kong' ),
-( 1, 'Gordon Freeman' ),
-( 1, 'Adam Jensen' ),
-( 1, 'The Nameless One' ),
-( 1, 'Guybrush Threepwood' );
+INSERT INTO players_list (`faf_id`, `status`,  `player_name`) VALUES
+(1,  1, 'Lara Croft' ),
+(2,  1, 'Max Payne' ),
+(3,  1, 'Donkey Kong' ),
+(4,  1, 'Gordon Freeman' ),
+(5,  1, 'Adam Jensen' ),
+(6,  1, 'The Nameless One' ),
+(7,  1, 'Guybrush Threepwood' );
 
 -- assumes the database tables have been created and populated from scratch, otherwise hardcoded IDs will not be correct.
 INSERT INTO clan_members ( clan_id, player_id, clan_rank ) VALUES 
