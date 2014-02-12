2826bea83f7f6b9c5c1a358dc2aaa83446e6d68f
Dragonfire <bastian616-git@yahoo.de>
2014-02-12 22:50:10 +0100

    invite players backend TODO: check user input

---
+++ app/component/_invite_player.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+
+Clan Details page component
+
+**/
+
+namespace App\Component;
+
+class _Invite_Player extends \App\Controller {
+    
+    function get($f3, $f3_request_info = null) {
+		$inv_player = $f3->get('GET.player');
+		
+		$player = $f3->get( 'logged_in_player' );	
+		$f3->get( 'DB_CLANS' );
+		
+		$db = $f3->get( 'DB_CLANS' );
+    
+		$result = $db->exec( 'SELECT clan_id FROM clan_members WHERE player_id = ?', $player->player_id);	
+		$clan_id = $result[0]['clan_id'];
+		
+		$clan_member = new \Model\Clan_Invites($db);
+		$clan_member->addInvite($inv_player, $clan_id);
+
+        die();
+    }
+    
+}
