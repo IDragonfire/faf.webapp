aeaa2c044dd09c006ab50fc66a45e8dd8b2bf4df
Dragonfire <bastian616-git@yahoo.de>
2014-02-12 18:33:11 +0100

    add invite players draft +jquery autocomplete plugin

---
+++ app/component/_invite_players.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+
+Clan Details page component
+
+**/
+
+namespace App\Component;
+
+class _Invite_Players extends \App\Controller {
+    
+    function get($f3, $f3_request_info = null) {
+		$player = $f3->get('GET.term');
+		if(count($player) < 1) {
+			die();
+		}
+		
+		$player_list = new \Model\Players_List( $f3->get( 'DB_CLANS' ) );
+		$players = $player_list->find( array( "player_name LIKE concat('%',?,'%')", $player) ); 
+		$data = array();
+		foreach($players as $p) {
+			 $data[] = array('id' => $p->player_name, 'value' => $p->player_name, 'label' => $p->player_name);
+		}
+		
+		$json = json_encode($data);
+		print $json;
+		die();
+		
+        if ($f3_request_info) {
+            
+            // component called as AJAX    
+            $template = new \Template;
+            echo $template->render('_invite_players.htm');
+            die();
+        } else {
+            // component called internally
+        }
+        
+    }
+    
+}
