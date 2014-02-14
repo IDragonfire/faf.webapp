44e9191247f76bea1819b46e6af7a94fb1f2202f
Dragonfire <bastian616-git@yahoo.de>
2014-02-14 21:13:13 +0100

    add draft for permission system

---
+++ model/permission.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Model;
+
+class Permission  extends \DB\SQL\Mapper {
+	const MY_CLAN_REMOVE_MEMBER = 'my_clan_remvove_member';
+	const MY_CLAN_INVITE_PLAYER = 'my_clan_invite_member';
+	const MY_CLAN_HANDLE_MEMBERSHIP_REQUEST = 'my_clan_handle_membership_request';
+	const MY_CLAN_EDIT_DETAILS = 'my_clan_edit_details';
+
+	// Instantiate mapper
+	function __construct( \DB\SQL $db ) {
+		// This is where the mapper and DB structure synchronization occurs
+		parent::__construct( $db, 'clan_member');
+	}
+	
+	function hasPerm($player_id, $perm) {
+		return TRUE;
+	}
+}
