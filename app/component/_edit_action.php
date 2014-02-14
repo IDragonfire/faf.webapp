628fb45045f4dae65c2342e183f73217575646a2
Dragonfire <bastian616-git@yahoo.de>
2014-02-14 08:04:59 +0100

    implement backend #5

--- app/component/_edit_action.php
+++ app/component/_edit_action.php
@@ -3,46 +3,40 @@ namespace App\Component;
 
 class _Edit_Action extends \App\Controller {
     
-    function post($f3, $f3_request_info = null) {
-
-   
-    $pk = $f3->get( 'POST.pk');
-    $name = $f3->get( 'POST.name');
-    $value = $f3->get( 'POST.value');
-
-	
-	if($pk == 'clan_tag') {
-		if(strlen($value) > 3) {
-			header('HTTP 400 Bad Request', true, 400);
-			echo "Max length is 3!";
+    function post($f3, $f3_request_info = null) {   
+		$pk = $f3->get( 'POST.pk');
+		$value = $f3->get( 'POST.value');
+		
+		$db = $f3->get( 'DB_CLANS');
+		
+		# TODO; check also on creation, make one unit
+		if($pk == 'clan_tag') {
+			if(strlen($value) > 3) {
+				$this->error('Maximum 3 characters!');
+			}
+			$mapper = new \Model\Clans_List($db);
+			$mapper->load( array( ' clan_id = ? ', $this->getClanId($f3) ) );
+			$mapper->clan_tag = $value;
+			$mapper->save();
 			die();
 		}
-	}
-    /*
-     Check submitted value
-    */
-    if(!empty($value)) {
-        /*
-          If value is correct you process it (for example, save to db).
-          In case of success your script should not return anything, standard HTTP response '200 OK' is enough.
-          
-          for example:
-          $result = mysql_query('update users set '.mysql_escape_string($name).'="'.mysql_escape_string($value).'" where user_id = "'.mysql_escape_string($pk).'"');
-        */
-        
-        //here, for debug reason we just return dump of $_POST, you will see result in browser console
-        print_r($_POST);
-
-
-    } else {
-        /* 
-        In case of incorrect value or error you should return HTTP status != 200. 
-        Response body will be shown as error message in editable form.
-        */
-
-         header('HTTP 400 Bad Request', true, 400);
-        echo "This field is required!";
+		# TODO: check also on creation, make one unit
+		if($pk == 'clan_name') {
+			if(strlen($value) < 4) {
+				$this->error('Minimum 4 characters!');
+			}
+			$mapper = new \Model\Clans_List($db);
+			$mapper->load( array( ' clan_id = ? ', $this->getClanId($f3) ) );
+			$mapper->clan_name = $value;
+			$mapper->save();
+			die();
+		}
+		$this->error('invalid primary key');
+    }
+	
+	function error($msg) {
+		header('HTTP 400 Bad Request', true, 400);
+		echo $msg;
 		die();
-    }
-    }
+	}
 }
