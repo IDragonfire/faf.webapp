07fcc1520788d1fd0943655bb0c69addfc8a443f
Dragonfire <bastian616-git@yahoo.de>
2014-02-15 23:18:55 +0100

    check remove member perm fixed #26

--- app/component/_clan_action.php
+++ app/component/_clan_action.php
@@ -14,14 +14,20 @@ class _Clan_Action extends \App\Controller {
 		$action = $f3->get( 'GET.action' );	
 		if($action == 'remove') {
 			$player_id = $f3->get( 'GET.player' );
+			$db = $f3->get( 'DB_CLANS' );
+			$current_player_id = $f3->get( 'logged_in_player' )->player_id;
 			// if we try to edit ourself
-			if($player_id == $f3->get( 'logged_in_player' )->player_id) {
+			if($player_id == $current_player_id) {
 				echo 'you cannot edit yourself';
 				die();
 			}
-			# TODO: check if player exists
-			# TODO: check permission of initiator
-			$clan_members = new \Model\Clan_Member( $f3->get( 'DB_CLANS' ) );
+			// if player has no permission
+	        $perm = new \Model\Permission($db);
+	        if(!$perm->hasPerm($current_player_id, \Model\Permission::MY_CLAN_REMOVE_MEMBER)) {
+	            echo 'no perm';
+	            die();
+	        }
+			$clan_members = new \Model\Clan_Member( $db );
 			$clan_members->leaveClan($player_id);
 			$f3->reroute('/my_clan');
 		}
