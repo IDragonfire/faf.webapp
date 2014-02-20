b7077950b4c64feac3147db7de25ec1fc9cc10e5
Dragonfire <bastian616-git@yahoo.de>
2014-02-20 18:07:01 +0100

    beautify invite player code

--- app/component/_invite_player.php
+++ app/component/_invite_player.php
@@ -9,22 +9,28 @@ Clan Details page component
 namespace App\Component;
 
 class _Invite_Player extends \App\Controller {
-    
-    function get($f3, $f3_request_info = null) {
+
+	function get($f3, $f3_request_info = null) {
 		$inv_player = $f3->get('GET.player');
-		
-		$player = $f3->get( 'logged_in_player' );	
-		$f3->get( 'DB_CLANS' );
+		if($inv_player <= 0) {
+			echo 'not a valid player';
+		}
 		
 		$db = $f3->get( 'DB_CLANS' );
-    
-		$result = $db->exec( 'SELECT clan_id FROM clan_members WHERE player_id = ?', $player->player_id);	
-		$clan_id = $result[0]['clan_id'];
+
+		// if player has no permission
+		$perm = new \Model\Permission($db);
+		if(!$perm->hasPerm($this->player_id, \Model\Permission::MY_CLAN_INVITE_PLAYER)) {
+			echo 'no perm';
+			die();
+		}
+
+		$clan_id = $this->getClanId($f3);
 		
 		$clan_member = new \Model\Clan_Invites($db);
 		$clan_member->addInvite($inv_player, $clan_id, \Model\Clan_Invites::INVITE_LEADER_REQUEST);
 
-        die();
-    }
-    
+		die();
+	}
+
 }
