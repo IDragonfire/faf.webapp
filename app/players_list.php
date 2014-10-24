a2519af53721df7825a20d4cf61a6ab6c461f367
Dragonfire <bastian616-git@yahoo.de>
2014-10-24 18:59:38 +0200

    clean up player list

--- app/players_list.php
+++ app/players_list.php
@@ -1,25 +1,16 @@
 <?php
-
 namespace App;
 
 class Players_List extends Controller {
 
 	function get($f3) {
+        // set to nav menu shows correct selected item
+		$f3->set( 'selected_page', 'players_list' );
 
-    // set to nav menu shows correct selected item
-    $f3->set( 'selected_page', 'players_list' );
+		$players_list_mapper_arr =  $f3->get( 'DB_CLANS' )->exec( 'SELECT * FROM player_list_page_view');
+		$f3->set( 'players_list_arr', $players_list_mapper_arr );
 
-    $tbl_players_list = new \Model\Players_List( $f3->get( 'DB_CLANS' ) );
-    
-     $players_list_mapper_arr =  $f3->get( 'DB_CLANS' )->exec( 'SELECT * FROM player_list_page_view');
-    
-	  $f3->set( 'players_list_arr', $players_list_mapper_arr );
-	
-	
-	  // page content
-    $f3->set('main_content_template', '_players_list.htm'); 
-	
-	
-	
+	 	// page content
+		$f3->set('main_content_template', '_players_list.htm'); 
 	}
 }
